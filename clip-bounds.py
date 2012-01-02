5fa90106ea2df07b1766613ebfb3941905e994f2
Michal Migurski <migurski@li155-150.members.linode.com>
2012-01-02 05:46:31 +0000

    Pushed database parts to the end

--- clip-bounds.py
+++ clip-bounds.py
@@ -77,69 +77,65 @@ metro_pattern = 'http://osm-metro-extracts.s3.amazonaws.com/%s.osm.pbf'
 gf_url = 'http://download.geofabrik.de/clipbounds/clipbounds.tgz'
 gf_base_href = 'http://download.geofabrik.de/osm/'
 
-if __name__ != '__main__':
-    # don't import me, I'm expensive
-    exit()
+if __name__ == '__main__':
 
-
-
-extracts = []
-
-metro_list = StringIO(urlopen(metro_url).read())
-metro_list = DictReader(metro_list, dialect='excel-tab')
-
-for city in metro_list:
-    extract_href = metro_pattern % city['slug']
+    extracts = []
     
-    try:
-        content_length, last_modified = size_and_date(extract_href)
-        print extract_href
-
-    except IOError:
-        print 'Failed', extract_href
-        continue
+    metro_list = StringIO(urlopen(metro_url).read())
+    metro_list = DictReader(metro_list, dialect='excel-tab')
     
-    y1, x1, y2, x2 = [float(city[key]) for key in ('top', 'left', 'bottom', 'right')]
-    shape = Polygon([(x1, y1), (x2, y1), (x2, y2), (x1, y2), (x1, y1)])
+    for city in metro_list:
+        extract_href = metro_pattern % city['slug']
+        
+        try:
+            content_length, last_modified = size_and_date(extract_href)
+            print extract_href
     
-    extracts.append((extract_href, content_length, last_modified, shape))
-
-
-
-gf_archive = StringIO(urlopen(gf_url).read())
-gf_archive = GzipFile(fileobj=gf_archive)
-gf_archive = TarFile(fileobj=gf_archive)
-
-for member in gf_archive.getmembers():
-    if not member.name.endswith('.poly'):
-        continue
+        except IOError:
+            print 'Failed', extract_href
+            continue
+        
+        y1, x1, y2, x2 = [float(city[key]) for key in ('top', 'left', 'bottom', 'right')]
+        shape = Polygon([(x1, y1), (x2, y1), (x2, y2), (x1, y2), (x1, y1)])
+        
+        extracts.append((extract_href, content_length, last_modified, shape))
     
-    extract_path = member.name[:-5] + '.osm.pbf'
-    extract_href = urljoin(gf_base_href, extract_path)
     
-    try:
-        content_length, last_modified = size_and_date(extract_href)
-        print extract_href
-
-    except IOError:
-        print 'Failed', extract_href
-        continue
     
-    lines = list(gf_archive.extractfile(member))
-    shape = parse_poly(lines)
+    gf_archive = StringIO(urlopen(gf_url).read())
+    gf_archive = GzipFile(fileobj=gf_archive)
+    gf_archive = TarFile(fileobj=gf_archive)
     
-    extracts.append((extract_href, content_length, last_modified, shape))
-
-
-
-db = connect(database='tiledrawer', user='tiledrawer').cursor()
-
-db.execute('BEGIN')
-db.execute('DELETE FROM extracts')
-
-for (href, size, date, shape) in extracts:
-    db.execute("""INSERT INTO extracts (href, size, date, geom)
-                  VALUES (%s, %s, %s, SetSRID(Multi(GeomFromText(%s)), 4326))""",
-               (href, size, date, str(shape)))
-
-db.execute('COMMIT')
+    for member in gf_archive.getmembers():
+        if not member.name.endswith('.poly'):
+            continue
+        
+        extract_path = member.name[:-5] + '.osm.pbf'
+        extract_href = urljoin(gf_base_href, extract_path)
+        
+        try:
+            content_length, last_modified = size_and_date(extract_href)
+            print extract_href
+    
+        except IOError:
+            print 'Failed', extract_href
+            continue
+        
+        lines = list(gf_archive.extractfile(member))
+        shape = parse_poly(lines)
+        
+        extracts.append((extract_href, content_length, last_modified, shape))
+    
+    
+    
+    db = connect(database='tiledrawer', user='tiledrawer').cursor()
+    
+    db.execute('BEGIN')
+    db.execute('DELETE FROM extracts')
+    
+    for (href, size, date, shape) in extracts:
+        db.execute("""INSERT INTO extracts (href, size, date, geom)
+                      VALUES (%s, %s, %s, SetSRID(Multi(GeomFromText(%s)), 4326))""",
+                   (href, size, date, str(shape)))
+    
+    db.execute('COMMIT')
